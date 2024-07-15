21ef2ba99a5d222d4ce1a4d5579a74439a432da6
KartikSindura <kartik.sindura@gmail.com>
2024-07-15 16:15:57 +0530

    fix: no comments check

--- components/Commentbox.js
+++ components/Commentbox.js
@@ -33,98 +33,9 @@ export default function Commentbox({ videoId }) {
     fetch();
   }, []);
 
-  return (
-    <View>
-      {!loading ? (
-        <View>
-          <RBSheet
-            closeOnPressBack={true}
-            draggable={false}
-            ref={bottomSheet}
-            height={628}
-            customStyles={{
-              wrapper: {
-                backgroundColor: "transparent",
-              },
-              draggableIcon: {
-                backgroundColor: themecolors.categories,
-              },
-              container: {
-                backgroundColor: themecolors.bg,
-              },
-            }}
-            customModalProps={{
-              animationType: "slide",
-              statusBarTranslucent: true,
-            }}
-            customAvoidingViewProps={{
-              enabled: false,
-            }}
-          >
-            <View className="flex-1">
-              <View className="flex-row justify-between p-4">
-                <Text className="text-white font-bold text-xl">Comments</Text>
-                <Pressable
-                  onPress={() => {
-                    bottomSheet.current.close();
-                  }}
-                >
-                  <Ionicons name="close" size={24} color="white" />
-                </Pressable>
-              </View>
-              <ScrollView className="flex-1 p-3">
-                {commentData &&
-                  commentData.data.map((item, index) => {
-                    return (
-                      <View className="flex-col mb-7">
-                        <View className="flex-row space-x-3 ">
-                          <Image
-                            source={{
-                              uri: item.authorProfileImageUrl[0].url,
-                            }}
-                            className="h-6 w-6 rounded-full "
-                          />
-                          <Text className="text-zinc-400 text-xs">
-                            {item.authorDisplayName} · {item.publishedTimeText}
-                          </Text>
-                        </View>
-                        <View className="">
-                          <Text className="text-white text-sm ml-10">
-                            {item.textDisplay}
-                          </Text>
-                        </View>
-                      </View>
-                    );
-                  })}
-              </ScrollView>
-            </View>
-          </RBSheet>
-
-          <Pressable
-            className="rounded-2xl p-3"
-            style={{ backgroundColor: themecolors.categories }}
-            onPress={() => bottomSheet.current.open()}
-          >
-            <View className="flex-row space-x-1 items-center mb-2">
-              <Text className="text-white">Comments</Text>
-              <Text className="text-zinc-400 text-xs">
-                {commentData.commentsCount}
-              </Text>
-            </View>
-            <View className="flex-row items-center space-x-3">
-              <Image
-                source={{
-                  uri: commentData.data[0].authorProfileImageUrl[0].url,
-                }}
-                className="h-6 w-6 rounded-full"
-              />
-              <Text className="text-white text-xs flex-1">
-                {commentData.data[0].textDisplay}
-              </Text>
-            </View>
-          </Pressable>
-        </View>
-      ) : (
+  function check() {
+    if (loading) {
+      return (
         <View
           className="rounded-2xl p-3"
           style={{ backgroundColor: themecolors.categories }}
@@ -137,7 +48,116 @@ export default function Commentbox({ videoId }) {
             <View className="rounded-2xl h-5 w-80 bg-zinc-800"></View>
           </View>
         </View>
-      )}
-    </View>
-  );
+      );
+    }
+
+    if (commentData.data.length == 0) {
+      return (
+        <View
+          className="rounded-2xl p-3"
+          style={{ backgroundColor: themecolors.categories }}
+        >
+          <View className="flex-row space-x-1 items-center mb-3">
+            <Text className="text-white">Comments</Text>
+          </View>
+          <View className="flex-row items-center space-x-3">
+            <Text className="text-white text-xs flex-1">No comments yet</Text>
+          </View>
+        </View>
+      );
+    }
+
+    return (
+      <View>
+        <RBSheet
+          closeOnPressBack={true}
+          draggable={false}
+          ref={bottomSheet}
+          height={628}
+          customStyles={{
+            wrapper: {
+              backgroundColor: "transparent",
+            },
+            draggableIcon: {
+              backgroundColor: themecolors.categories,
+            },
+            container: {
+              backgroundColor: themecolors.bg,
+            },
+          }}
+          customModalProps={{
+            animationType: "slide",
+            statusBarTranslucent: true,
+          }}
+          customAvoidingViewProps={{
+            enabled: false,
+          }}
+        >
+          <View className="flex-1">
+            <View className="flex-row justify-between p-4">
+              <Text className="text-white font-bold text-xl">Comments</Text>
+              <Pressable
+                onPress={() => {
+                  bottomSheet.current.close();
+                }}
+              >
+                <Ionicons name="close" size={24} color="white" />
+              </Pressable>
+            </View>
+            <ScrollView className="flex-1 p-3">
+              {commentData.data &&
+                commentData.data.map((item, index) => {
+                  return (
+                    <View className="flex-col mb-7">
+                      <View className="flex-row space-x-3 ">
+                        <Image
+                          source={{
+                            uri: item.authorProfileImageUrl[0].url,
+                          }}
+                          className="h-6 w-6 rounded-full "
+                        />
+                        <Text className="text-zinc-400 text-xs">
+                          {item.authorDisplayName} · {item.publishedTimeText}
+                        </Text>
+                      </View>
+                      <View className="">
+                        <Text className="text-white text-sm ml-10">
+                          {item.textDisplay}
+                        </Text>
+                      </View>
+                    </View>
+                  );
+                })}
+            </ScrollView>
+          </View>
+        </RBSheet>
+
+        <Pressable
+          className="rounded-2xl p-3"
+          style={{ backgroundColor: themecolors.categories }}
+          onPress={() => bottomSheet.current.open()}
+        >
+          <View className="flex-row space-x-1 items-center mb-2">
+            <Text className="text-white">Comments</Text>
+            <Text className="text-zinc-400 text-xs">
+              {commentData.commentsCount}
+            </Text>
+          </View>
+          <View className="flex-row items-center space-x-3">
+            <Image
+              source={{
+                uri: commentData.data[0].authorProfileImageUrl[0].url,
+              }}
+              className="h-6 w-6 rounded-full"
+            />
+            <Text className="text-white text-xs flex-1">
+              {commentData.data[0].textDisplay}
+            </Text>
+          </View>
+        </Pressable>
+      </View>
+    );
+  }
+
+  return <View>{check()}</View>;
 }
